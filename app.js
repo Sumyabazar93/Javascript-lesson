4d2ec744753475c86bad3bc7efebd874c168870a
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-12 15:39:47 +0800

    Bind, Call, Apply

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var img = {
     file: "c:/images/a123.jpg",
     content: 'muur',
-    zuragTanih: function () {
-        console.log("Ene zurag deer " + this.content + " baigaa n tanigdlaa");
+    zuragTanih: function (color, size) {
+        console.log("Ene zurag deer " + color + " ungutei " + size + "  hemjeetei " + this.content + " baigaa n tanigdlaa");
     }
 }
-img.zuragTanih();
+img.zuragTanih('har', 'bytshan');
 
 var myImg = {
     file: "c:/images/a541.jpg",
     content: 'nohoi',
 }
+// Bind
 var mySuperZuragTanihFunction = img.zuragTanih.bind(myImg);
-
 mySuperZuragTanihFunction();
+
+// Call
+
+img.zuragTanih.call(myImg, 'nogoon', 'dund zergiin');
+
+// Apply
+
+var args = ['ygaan', 'avarga'];
+img.zuragTanih.apply(myImg, args);
