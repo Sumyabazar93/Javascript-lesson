ae8f969205fd437cd8d18667d0dfcff4c8c90c74
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-12 15:29:05 +0800

    Prototype function lesson 53

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+function Animal(ner) {
+    this.name = ner;
+
+};
+Animal.prototype.hoolloh = function () {
+    console.log(this.name + " hoolloloo");
+};
+
+var a1 = new Animal('Ymaa');
+a1.mailah = function () {
+    console.log(this.name + " Maillaa");
+}
+
+a1.hoolloh();
+a1.mailah();
+
+var a2 = new Animal('Uher');
+a2.hoolloh();
+
+var a3 = new Animal('Aduu');
+a3.hoolloh();
+*/
+
+function Animal() {
+    // this.name = ner;
+
+};
+Animal.prototype.hoolloh = function () {
+    console.log(" hoolloloo");
+};
+
+var a1 = new Animal();
+
+a1.hoolloh();
