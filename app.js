3621b56cccd192f72f52f5153c64900b4feb2279
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-13 15:10:10 +0800

    Materi.call(this, ...) iig oilgoloo

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-var img = {
-    file: "c:/images/a123.jpg",
-    content: 'muur',
-    zuragTanih: function (color, size) {
-        console.log("Ene zurag deer " + color + " ungutei " + size + "  hemjeetei " + this.content + " baigaa n tanigdlaa");
-    }
+function Materi(une, ungu) {
+    this.ungu = ungu;
+    this.une = une;
 }
-img.zuragTanih('har', 'bytshan');
-
-var myImg = {
-    file: "c:/images/a541.jpg",
-    content: 'nohoi',
+Materi.prototype.haragdah = function () {
+    console.log(this.une + " Unetei materi");
 }
-// Bind
-var mySuperZuragTanihFunction = img.zuragTanih.bind(myImg);
-mySuperZuragTanihFunction();
 
-// Call
+// var m1 = new Materi(1500, "red");
 
-img.zuragTanih.call(myImg, 'nogoon', 'dund zergiin');
+function Hun(ner, nas, undur, huis, une, ungu){
+    Materi.call(this, une, ungu);
+    this.ner = ner;
+    this.nas = nas;
+    this.undur = undur;
+    this.huis = huis;
+}
+Hun.prototype = Object.create(Materi.prototype);
+Hun.prototype.ideh = function (hool) {
+    this.hool = hool;
+    console.log(this.ner + " nertei " + this.nas + " nastai " + this.undur + " undurtei " + this.huis + " huistei hun " + this.hool + " hool idlee " + this.une + " amitai " + this.ungu + " hun");
+}
 
-// Apply
+function Amitan(nas, une, ungu) {
+    Materi.call(this, une, ungu);
+    this.nas = nas;
+    // var arr = [1500, 'red'];
+    // Materi.apply(this, arr);
+}
+Amitan.prototype = Object.create(Materi.prototype);
+Amitan.prototype.hoolloh = function (hool) {
+    this.hool = hool;
+    console.log(this.nas + ' Nastai ' + this.une + " unetei " + this.ungu + ' ungutei amitan hoololooo' + this.hool + " idlee");
+}
+var unee = new Amitan(20, 40000, 'alag');
+var ymaa = new Amitan(5, 80000, 'black');
+var hun = new Hun("Bold", 42, 189, 'ER', 7, 'bor');
 
-var args = ['ygaan', 'avarga'];
-img.zuragTanih.apply(myImg, args);
+hun.ideh("Buuz");
+ymaa.hoolloh('hog');
+unee.haragdah();
