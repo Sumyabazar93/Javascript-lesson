e78299fd81cd2ba2e8f24323b94be18ab4541a97
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-13 16:50:10 +0800

    function hylbar herglee

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
-function Materi(une, ungu) {
-    this.ungu = ungu;
-    this.une = une;
+// var math = {
+//     abs: function (too) {
+//         if (too < 0) return 0 - too;
+//         else return too;
+//     }
+// }
+// console.log(math.abs(-2250));
+
+var birthYears = [1981, 2005, 1978, 1986, 2010];
+
+// console.log(findOutAges());
+// console.log(ages);
+// console.log(isFullAges);
+
+// var ages = findOutAges();
+// function findOutAges() {
+//     var ages = [];
+//     for (var i = 0; i < birthYears.length; i++) {
+//         ages.push(new Date().getFullYear() - birthYears[i]);
+//     }
+//     return ages;
+// }
+
+// var isFullAges = isFullAge();
+// function isFullAge() {
+//     var isFullAges = [];
+//     for (var i = 0; i < ages.length; i++) {
+//         isFullAges.push(ages[i] >= 18);
+//     }
+//     return isFullAges;
+// }
+
+var ages = processArray(birthYears, findOutAges);
+
+console.log(ages);
+console.log(processArray(ages, isFullAge));
+
+function processArray(myArr, fn) {
+    var arr = [];
+
+    for (var i = 0; i < myArr.length; i++) {
+        arr.push(fn(myArr[i]));
+    }
+    return arr;
 }
-Materi.prototype.haragdah = function () {
-    console.log(this.une + " Unetei materi");
+function findOutAges(el) {
+    return new Date().getFullYear() - el;
 }
 
-// var m1 = new Materi(1500, "red");
-
-function Hun(ner, nas, undur, huis, une, ungu){
-    Materi.call(this, une, ungu);
-    this.ner = ner;
-    this.nas = nas;
-    this.undur = undur;
-    this.huis = huis;
+function isFullAge(el) {
+    return el >= 18;
 }
-Hun.prototype = Object.create(Materi.prototype);
-Hun.prototype.ideh = function (hool) {
-    this.hool = hool;
-    console.log(this.ner + " nertei " + this.nas + " nastai " + this.undur + " undurtei " + this.huis + " huistei hun " + this.hool + " hool idlee " + this.une + " amitai " + this.ungu + " hun");
-}
-
-function Amitan(nas, une, ungu) {
-    Materi.call(this, une, ungu);
-    this.nas = nas;
-    // var arr = [1500, 'red'];
-    // Materi.apply(this, arr);
-}
-Amitan.prototype = Object.create(Materi.prototype);
-Amitan.prototype.hoolloh = function (hool) {
-    this.hool = hool;
-    console.log(this.nas + ' Nastai ' + this.une + " unetei " + this.ungu + ' ungutei amitan hoololooo' + this.hool + " idlee");
-}
-var unee = new Amitan(20, 40000, 'alag');
-var ymaa = new Amitan(5, 80000, 'black');
-var hun = new Hun("Bold", 42, 189, 'ER', 7, 'bor');
-
-hun.ideh("Buuz");
-ymaa.hoolloh('hog');
-unee.haragdah();
