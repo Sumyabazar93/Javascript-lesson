4d93cb06d1c914c2c2f5b42218e25e8e0f7ed865
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-13 17:09:03 +0800

    funktsees funktsiig butsaah

--- app.js
+++ app.js
@@ -1,57 +1,13 @@
-// var math = {
-//     abs: function (too) {
-//         if (too < 0) return 0 - too;
-//         else return too;
-//     }
-// }
-// console.log(math.abs(-2250));
+var question = [];
+question['software-engineer'] = ['Ta medhu'];
+question['teacher'] = ['Ta hedn huuhedtei ve'];
+question['driver'] = ['Ta ymr mashintai ve'];
 
-
-// console.log(findOutAges());
-// console.log(ages);
-// console.log(isFullAges);
-
-// var ages = findOutAges();
-// function findOutAges() {
-//     var ages = [];
-//     for (var i = 0; i < birthYears.length; i++) {
-//         ages.push(new Date().getFullYear() - birthYears[i]);
-//     }
-//     return ages;
-// }
-
-// var isFullAges = isFullAge();
-// function isFullAge() {
-//     var isFullAges = [];
-//     for (var i = 0; i < ages.length; i++) {
-//         isFullAges.push(ages[i] >= 18);
-//     }
-//     return isFullAges;
-// }
-
-var birthYears = [1981, 2005, 1950, 1986, 1948];
-
-var ages = processArray(birthYears, findOutAges);
-
-console.log(ages);
-console.log(processArray(ages, isFullAge));
-
-console.log(processArray(ages, function (el) {
-    return el >= 65;
-}));
-
-
-function processArray(myArr, fn) {
-    var arr = [];
-
-    for (var i = 0; i < myArr.length; i++) {
-        arr.push(fn(myArr[i]));
+function interviewQuestion(job) {
+    return function (name) {
+        console.log(name + ', ' + question[job]);
     }
-    return arr;
 }
-function findOutAges(el) {
-    return new Date().getFullYear() - el;
-}
-function isFullAge(el) {
-    return el >= 18;
-}
+interviewQuestion('software-engineer')('Mr.DJ');
+interviewQuestion('teacher')('Mr.DJ');
+interviewQuestion('driver')('Mr.DJ');
