3c4aaf689e6e213bda3b6a3c8c3fd4e9b52f6e21
Sumyabazar93 <sumyabazar93@gmail.com>
2022-12-14 14:26:00 +0800

    shuud funkts

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-var question = [];
-question['software-engineer'] = ['Ta medhu'];
-question['teacher'] = ['Ta hedn huuhedtei ve'];
-question['driver'] = ['Ta ymr mashintai ve'];
+// var a = [];
 
-function interviewQuestion(job) {
-    return function (name) {
-        console.log(name + ', ' + question[job]);
+// a.push(34);
+// a.push("hello");
+// a.push(function () {
+//     console.log("Sainu massiv dotor");
+
+//     return function () {
+//         console.log('Massiv dotorh funkts dotorh');
+//         return function () {
+//             console.log('Inception!!!');
+//         }
+//     }
+// })
+// console.log(a);
+// console.log(a[2]()()());
+
+function prepare() {
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            (function (j) {
+                return function () {
+                    console.log(j);
+                };
+            })(i)
+        );
     }
+    return arr;
 }
-interviewQuestion('software-engineer')('Mr.DJ');
-interviewQuestion('teacher')('Mr.DJ');
-interviewQuestion('driver')('Mr.DJ');
+
+var massive = prepare();
+
+massive[0]();
+massive[1]();
+massive[2]();
+
+console.log(massive);
